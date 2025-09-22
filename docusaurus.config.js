4e77ce75f885d7f9fd5b516f57914f53de582476
unknown <user@WINDOWS-R2V3M6T.cdsafi.com>
2025-09-22 09:51:41 +0800

    fix url and repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,15 +21,18 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  // url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'askdataisland-tw', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
+
+   // 建議指定發佈分支
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
